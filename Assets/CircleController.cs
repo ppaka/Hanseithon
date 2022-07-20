de58af8c6e684ae0f28e11c69f1b68f4ef5faa27
ppaka <asj0216@gmail.com>
2022-07-20 16:16:37 +0900

    서클 이동

--- Assets/CircleController.cs
+++ Assets/CircleController.cs
@@ -1,18 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class CircleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform[] starPoints;
+    public Transform circle;
+    private Sequence _sequence;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        circle.position = starPoints[0].position;
+        _sequence = DOTween.Sequence()
+            .Insert(0, circle.DOMove(starPoints[1].position, 0.5f))
+            .Insert(0.5f, circle.DOMove(starPoints[2].position, 0.5f))
+            .Insert(1f, circle.DOMove(starPoints[3].position, 0.5f))
+            .Insert(1.5f, circle.DOMove(starPoints[4].position, 0.5f))
+            .Insert(2, circle.DOMove(starPoints[5].position, 0.5f))
+            .Insert(2.5f, circle.DOMove(starPoints[6].position, 0.5f))
+            .Insert(3, circle.DOMove(starPoints[7].position, 0.5f))
+            .Insert(3.5f, circle.DOMove(starPoints[8].position, 0.5f))
+            .Insert(4, circle.DOMove(starPoints[9].position, 0.5f))
+            .Insert(4.5f, circle.DOMove(starPoints[0].position, 0.5f))
+            .SetLoops(-1, LoopType.Restart);
+
+        _sequence.Play();
     }
 }
