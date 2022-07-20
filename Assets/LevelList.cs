b45cdc2e0d7fd7ce4154ab82371c95e4f58243c7
ppaka <asj0216@gmail.com>
2022-07-21 05:23:03 +0900

    타이틀, 메인화면

---
+++ Assets/LevelList.cs
@@ -0,0 +1,30 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class LevelList : MonoBehaviour
+{
+    private int _count;
+    public RectTransform leftTf, rightTf;
+    public RectTransform body;
+    public string[] titles;
+    public TMP_Text titleText;
+
+    public void MoveLeft()
+    {
+        if (_count == 0) return;
+        _count = 0;
+        body.DOKill();
+        body.DOMoveX(leftTf.transform.position.x, 0.5f).SetEase(Ease.OutQuart).Play();
+        titleText.text = titles[_count];
+    }
+
+    public void MoveRight()
+    {
+        if (_count == 1) return;
+        _count = 1;
+        body.DOKill();
+        body.DOMoveX(rightTf.transform.position.x, 0.5f).SetEase(Ease.OutQuart).Play();       
+        titleText.text = titles[_count];
+    }
+}
