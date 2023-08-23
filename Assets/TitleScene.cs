ab8d7622bb22754d6af44b51a0f0a53b22a9b8ea
ppaka <asj0216@gmail.com>
2023-08-24 00:21:01 +0900

    WebGL 플랫폼일 경우 나가기 버튼 비활성화

--- Assets/TitleScene.cs
+++ Assets/TitleScene.cs
@@ -1,12 +1,21 @@
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class TitleScene : MonoBehaviour
 {
+    public GameObject exitButton;
+    
     private void Awake()
     {
         Application.targetFrameRate = 60;
+    }
+
+    private void Start()
+    {
+        if (Application.platform == RuntimePlatform.WebGLPlayer)
+        {
+            exitButton.SetActive(false);
+        }
     }
 
     public void OpenStageScene()
